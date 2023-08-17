bc364352f1086a8fc6cc916679d0257a80935bcb
AapoPatjas1 <129507899+AapoPatjas1@users.noreply.github.com>
2023-08-17 12:18:32 +0300

    1.3 valmis

--- src/App.js
+++ src/App.js
@@ -8,43 +8,57 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
     <div>
-      <Part lol={props.part1} kukka={props.sigmaballs} />
-      <Part lol={props.part2} kukka={props.juhanishorman}/>
-      <Part lol={props.part3} kukka={props.poopypant}/>
+      <Part lol={ props.part1.name } kukka={props.part1.exercises} />
+      <Part lol={ props.part2.name } kukka={props.part2.exercises}/>
+      <Part lol={ props.part3.name } kukka={props.part3.exercises}/>
     </div>
   )
 }
 
+
 const Total = (props) => {
- return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  console.log(props)
+ return <p>Number of exercises {props.exercises1.exercises + props.exercises2.exercises + props.exercises3.exercises}</p>
 }
 
+const olionNimi = {
+    nimi: "sisältö"
+}
+console.log(olionNimi.nimi)
+
 const App = () => {
-  const otsikonTeksti = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development'
+
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
-      <Header otsikko={otsikonTeksti} />
+      <Header otsikko={course} />
       <Content 
       part1={part1}
       part2={part2}
       part3={part3}
-      sigmaballs={exercises1}
-      juhanishorman={exercises2}
-      poopypant={exercises3}
       />
       <Total 
-      exercises1={exercises1}
-      exercises2={exercises2}
-      exercises3={exercises3}
+      exercises1={part1}
+      exercises2={part2}
+      exercises3={part3}
       />
     </div>
   )
