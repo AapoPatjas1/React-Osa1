029a27b917c558847e4f5fc2be147eda6a2e53bd
AapoPatjas1 <129507899+AapoPatjas1@users.noreply.github.com>
2023-08-17 10:49:41 +0300

    tehtävä 1.1 valmis

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+const Header = (props) => {
+  return <h1> {props.otsikko} </h1>
+}
+
+
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+      {props.part1} {props.sigmaballs}
+    </p>
+    <p>
+      {props.part2} {props.juhanishorman}
+    </p>
+    <p>
+      {props.part3} {props.poopypant}
+    </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+ return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
+  const otsikonTeksti = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -9,17 +36,20 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header otsikko={otsikonTeksti} />
+      <Content 
+      part1={part1}
+      part2={part2}
+      part3={part3}
+      sigmaballs={exercises1}
+      juhanishorman={exercises2}
+      poopypant={exercises3}
+      />
+      <Total 
+      exercises1={exercises1}
+      exercises2={exercises2}
+      exercises3={exercises3}
+      />
     </div>
   )
 }
