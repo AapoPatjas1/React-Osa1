f60f31b18303b39478b7a22d3be12b6cfd474111
AapoPatjas1 <129507899+AapoPatjas1@users.noreply.github.com>
2023-08-21 09:47:10 +0300

    1.6 valmis

--- src/App.js
+++ src/App.js
@@ -1,68 +1,35 @@
-const Header = (props) => {
-  return <h1> {props.otsikko} </h1>
-}
-
-const Part = (props) => {
-  console.log(props)
-  return <p> {props.lol} {props.kukka} </p>
-}
-
-const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <Part lol={ props.part1.name } kukka={props.part1.exercises} />
-      <Part lol={ props.part2.name } kukka={props.part2.exercises}/>
-      <Part lol={ props.part3.name } kukka={props.part3.exercises}/>
-    </div>
-  )
-}
-
-
-const Total = (props) => {
-  console.log(props)
- return <p>Number of exercises {props.exercises1.exercises + props.exercises2.exercises + props.exercises3.exercises}</p>
-}
-
-const olionNimi = {
-    nimi: "sisältö"
-}
-console.log(olionNimi.nimi)
+import React, { useState } from 'react';
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <div>
-      <Header otsikko={course.name} />
-      <Content 
-      part1={course.parts[0]}
-      part2={course.parts[1]}
-      part3={course.parts[2]}
-      />
-      <Total 
-      exercises1={course.parts[0]}
-      exercises2={course.parts[1]}
-      exercises3={course.parts[2]}
-      />
-    </div>
-  )
-}
+      <h1>Unicafe Asiakaspalaute</h1>
+      <button onClick={handleGoodClick}>Hyvä</button>
+      <button onClick={handleNeutralClick}>Neutraali</button>
+      <button onClick={handleBadClick}>Huono</button>
 
-export default App
+      <h2>Palautteet</h2>
+      <p>Hyvä: {good}</p>
+      <p>Neutraali: {neutral}</p>
+      <p>Huono: {bad}</p>
+    </div>
+  );
+};
+
+export default App;
