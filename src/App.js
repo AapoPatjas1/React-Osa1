226ff592c95b4a44668026f7be4bf24f4ef9bfea
AapoPatjas1 <129507899+AapoPatjas1@users.noreply.github.com>
2023-08-28 12:17:48 +0300

    osa 2.1-2.3 valmis

--- src/App.js
+++ src/App.js
@@ -4,47 +4,41 @@ const Header = ({ otsikko }) => {
   return <h1>{otsikko}</h1>;
 };
 
-const Part = ({ lol, kukka }) => {
+const Part = (props) => {
   return (
     <p>
-      {lol} - {kukka} exercises
+      {props.nimi + " " + props.harjoitukset}
     </p>
   );
 };
 
-const Content = ({ part1, part2, part3 }) => {
-  return (
-    <div>
-      <Part lol={part1.name} kukka={part1.exercises} />
-      <Part lol={part2.name} kukka={part2.exercises} />
-      <Part lol={part3.name} kukka={part3.exercises} />
-    </div>
-  );
-};
+const Content = (props) => {
+  return(
+    <div>{props.course.parts.map(part => <Part nimi={part.name} harjoitukset={part.exercises} />)}</div>
+  )
+} 
 
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Number of exercises {totalExercises}</p>;
-};
+const Total = (props) => {
+  console.log(props)
+  return (
+	<p>Harjoitusten määrä  {props.course.parts.reduce((total, part) => total + part.exercises, 0)}</p>
+	)
+}
 
 const Course = (props) => {
-  return (
+  return(
     <div>
       <Header otsikko={props.course.name} />
-      <Content
-        part1={props.course.parts[0]}
-        part2={props.course.parts[1]}
-        part3={props.course.parts[2]}
-      />
-      <Total parts={props.course.parts} />
+      <Content course={props.course}/>
+      <Total course={props.course}/>
     </div>
-  );
-};
+  )
+}
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
-    id: 1,  
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
@@ -60,9 +54,14 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'State of a component aaa',
+        exercises: 142,
+        id: 4
       }
     ]
-  };
+  }
 
   return (
     <div>
