6b346705d7c0a01083646136f9381c6f0d9e6744
AapoPatjas1 <129507899+AapoPatjas1@users.noreply.github.com>
2023-08-29 12:14:27 +0300

    2.7 valmis

--- src/App.js
+++ src/App.js
@@ -1,58 +1,49 @@
-import React from 'react';
-import Course from './Course';
+import React, { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',  
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
 
+  const Nimi = (event) => { 
+    setNewName(event.target.value);
+  };
+
+  
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personExists = persons.some(person => person.name === newName);
+
+    if (personExists) {
+      alert(`${newName} is already in the phonebook.`);
+    } else {
+      const newPerson = { name: newName };
+      setPersons([...persons, newPerson]);
+      setNewName('');
+    }
+  };
 
   return (
     <div>
-      <Course course={courses} />
+      <h2>Puhelinkirja</h2>
+      <form onSubmit={addPerson}>
+      <div>
+      nimi: <input value={newName} onChange={Nimi} />
+     </div>
+   <div>
+     <button type="submit">Lisää</button>
+   </div>
+    </form>
+ <h2>Numerot</h2>
+     <ul>
+    {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
     </div>
-  );
-};
+  )
 
-export default App; 
+}
+
+export default App  
