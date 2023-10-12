570fe118c7f2779de11c258f091938ad67c381e8
AapoPatjas1 <129507899+AapoPatjas1@users.noreply.github.com>
2023-10-12 17:55:53 +0300

    2.11 valmis

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ searchQuery, handleSearchChange }) => (
   <div>
@@ -37,10 +38,19 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
+
+  
+  
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      });
+  }, []);
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
@@ -80,14 +90,22 @@ const App = () => {
 
   return (
     <div>
+
       <h2>Phonebook</h2>
+
       <Filter searchQuery={searchQuery} handleSearchChange={handleSearchChange} />
+
       <h3>Add a new</h3>
+      
       <PersonForm
         newName={newName}
+
         newNumber={newNumber}
+
         handleNameChange={handleNameChange}
+
         handleNumberChange={handleNumberChange}
+
         addPerson={addPerson}
       />
       <h3>Numbers</h3>
