3140da6d474d8961d2524567e4b2b5dcd2913f0c
AapoPatjas1 <129507899+AapoPatjas1@users.noreply.github.com>
2023-10-13 20:30:58 +0300

    2.12 valmis

--- src/App.js
+++ src/App.js
@@ -1,117 +1,122 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react'
 import axios from 'axios';
 
-const Filter = ({ searchQuery, handleSearchChange }) => (
-  <div>
-    Search: <input value={searchQuery} onChange={handleSearchChange} />
-  </div>
-);
+const checkValidity = (obj, arr) => {
+  for (let j = 0; j < arr.length; j++) {
+    if(arr[j].name === obj.name, arr[j].number === obj.number){
+      return true;
+    }
+  }
+  return false;
+}
 
-const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, addPerson }) => (
-  <form onSubmit={addPerson}>
-    <div>
-      name: <input value={newName} onChange={handleNameChange} />
-    </div>
-    <div>
-      number: <input value={newNumber} onChange={handleNumberChange} />
-    </div>
-    <div>
-      <button type="submit">Add</button>
-    </div>
-  </form>
-);
-
-const Persons = ({ filteredPersons }) => (
-  <div>
-    {filteredPersons.map((person, index) => (
-      <div key={index}>
-        {person.name} {person.number}
+const Filtered = (props) => {
+    return(
+      <div>
+        filter shown with <input type='text' onChange={props.func}/>
       </div>
-    ))}
-  </div>
-);
+    )
+}
 
+const PersonForm = (props) => {
+  return(
+    <div>
+      <div>
+          name: <input onChange={(e) => props.setNewName(e.target.value)}/><br/>
+          number: <input onChange={(e) => props.setNewNumber(e.target.value)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+    </div>
+  )
+}
+const Persons = (props) => {
+  return(
+    <div>
+      {
+        props.filter == "" ?
+          props.persons.map((person) =>
+          <p>{person.name  + " "}
+          {person.number}</p>)
+          :
+          props.filteredPersons.map((person) =>
+          <p>{person.name  + " "}
+          {person.number}</p>)
+         
+      }
+    </div>
+  )
+}
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
-
+  const [persons, setPersons] = useState([])
+  const [filter, setFilter] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filteredPersons, setFilteredPerson] = useState([])
   
-  
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [searchQuery, setSearchQuery] = useState('');
-
   useEffect(() => {
     axios.get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data);
-      });
+    .then(response => {
+      setPersons(response.data)
+    })
+    .catch(error => {
+      console.error("Error fetching data:", error)
+    })
   }, []);
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
+  const handleFilter = (e) => {
+    setFilter(e.target.value)
+    const result = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+    )
+    setFilteredPerson(result)
+  }
 
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const addPerson = (event) => {
-    event.preventDefault();
-    const personExists = persons.some(
-      (person) => person.name.toLowerCase() === newName.toLocaleLowerCase()
-    );
-    const personAxists = persons.some(
-      (persons) => persons.number.toLowerCase() === newNumber.toLocaleLowerCase()
-    );
-
-    if (personAxists) {
-      alert(`${newNumber} is already in the phonebook.`);
-    } else if (personExists) {
-      alert(`${newName} is already in the phonebook.`);
-    } else {
-      const newPerson = { id: persons.length + 1, name: newName, number: newNumber };
-      setPersons([...persons, newPerson]);
-      setNewName('');
-      setNewNumber('');
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const obj = {name: newName, number: newNumber};
+    if (checkValidity(obj, persons)) {
+      alert(`${newName} is already added to phonebook`)
+      alert(`${newNumber} is already added to phonebook`)
+    }else{
+      setPersons([...persons, obj])
     }
-  };
-
-  const filteredPersons = persons.filter((person) =>
-    person.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+    axios.post('http://localhost:3001/persons', obj)
+    .then (response => {
+      console.log('Data apended:', response.data);
+    })
+    .catch(error => {
+      console.error('Error appending data:', error);
+    })
+  }
 
   return (
     <div>
-
       <h2>Phonebook</h2>
 
-      <Filter searchQuery={searchQuery} handleSearchChange={handleSearchChange} />
-
-      <h3>Add a new</h3>
-      
-      <PersonForm
-        newName={newName}
-
-        newNumber={newNumber}
-
-        handleNameChange={handleNameChange}
-
-        handleNumberChange={handleNumberChange}
-
-        addPerson={addPerson}
+      <Filtered
+        func={handleFilter}
       />
-      <h3>Numbers</h3>
-      <Persons filteredPersons={filteredPersons} />
-    </div>
-  );
-};
+      <h2>add new</h2>
 
-export default App;
+      <form onSubmit={handleSubmit}>
+        <PersonForm 
+          setNewNumber={setNewNumber}
+          setNewName={setNewName}
+        />
+      </form>
+
+
+      <h2>Numbers</h2>
+      <Persons 
+        filteredPersons={filteredPersons}
+        filter={filter}
+        persons={persons}
+      />
+    </div>
+  )
+}
+
+
+export default App
